5b9a7e5d50b18d05e7ae0dfbd2cb0057c3553e72
Sam Sena <samsena@gmail.com>
2015-02-09 13:49:44 +0000

    lesson 1 end

---
+++ Lesson 1/fizzbuzz_final.py
@@ -0,0 +1,50 @@
+# you'll have to use control flow to deal with two situations: one in which the user supplies command line arguments, and one where they do not.
+
+import sys
+
+n = 100
+i = 0
+user_input = 0
+string = ""
+
+
+
+# if user doesn't input extra arguments
+# query user for input
+
+if len(sys.argv) == 1:
+  user_input = raw_input("Enter a number:")
+  try: 
+    i = int(user_input)
+  except ValueError:
+    print "Invalid character, please enter a number"
+  print "Fizz buzz counting up to {}".format(n)
+  # traps the error, need a loop, such as while true or false etc, keep prompting. now it takes i=0
+  while i < n:
+    if i % 3 == 0:
+      if i % 5 == 0:
+        print "fizz buzz"
+      else: 
+        print "fizz"
+    elif i % 5 == 0:
+      print "buzz"
+    else:
+      print i
+    i += 1
+else:
+  user_input = sys.argv[1]
+  i = int(user_input)
+  print i
+  print "Fizz buzz counting up to {}".format(n)
+  while i < n:
+    if i % 3 == 0:
+      if i % 5 == 0:
+        print "fizz buzz"
+      else: 
+        print "fizz"
+    elif i % 5 == 0:
+      print "buzz"
+    else:
+      print i
+    i += 1
+  
