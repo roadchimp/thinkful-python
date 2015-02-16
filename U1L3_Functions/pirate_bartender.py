46d6052e7855e94d0234ef24e2eae4b2a18f0ba2
Sam Sena <samsena@gmail.com>
2015-02-16 06:00:42 +0000

    pirate

--- U1L3_Functions/pirate_bartender.py
+++ U1L3_Functions/pirate_bartender.py
@@ -25,29 +25,46 @@ answers = {
   "fruity": [""]
 }
 
-def preferences(drink):
+test_input = ["Yes", "YeS", "yeS", "y", "YES"]
+test_output = [True, True, True, True, True]
+
+def preferences(value):
+  output = True
+  response = raw_input( "{}".format(value))
+  try: 
+    response.lower() not in ('yes', 'y', 'no', 'n')
+    print "Please enter Yes, Y, No or N."
+  except:
+    pass
+  if response.lower() == "yes":
+    answers[key] = True
+  elif response.lower() == "y":
+    answers[key] = True
+  elif response.lower() == "no":
+    answers[key] = False
+  elif response.lower() == "n":
+    answers[key] = False
+    output = answers[key]
+  return output
+
+def test_preferences():
+  for i, elem in enumerate(test_input):
+    test = preferences(elem)
+    control = test_output[i]
+    print "checking that preference({}) == {}; {}".format(elem, control, test)
+    assert(test == control)
+
+
+def main():
+  
+  #print test_preferences()
+  
   #do some kind of loop to iterate questions, then collect response in answers
   for (key, value) in questions.iteritems():
     while True:
-      response = raw_input( "{}".format(value))
-      if response.lower() not in ('yes', 'y', 'no', 'n'):
-        print "Please enter Yes, Y, No or N."
-      else:
-        break
-    if response.lower() == "yes":
-      answers[key] = True
-    elif response.lower() == "y":
-      answers[key] = True
-    elif response.lower() == "no":
-      answers[key] = False
-    elif response.lower() == "n":
-      answers[key] = False
-  return answers
-      
-print preferences(drink)
+      preferences(drink)
 
-#dictionary['name'] = 'neo'
-# print "{} => {}".format(key, value)
-# print questions["strong"]
-# answers = raw_input("Enter a number: ")
   
+print "__name__ is: ".format(__name__)
+if __name__ == "__main__":
+  main()
