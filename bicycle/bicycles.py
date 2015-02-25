3126ffa260780af3b75046059d7ae39485f18d29
Sam Sena <samsena@gmail.com>
2015-02-25 04:18:16 +0000

    Prior to dir restructure

--- bicycle/bicycles.py
+++ bicycle/bicycles.py
@@ -1,13 +1,49 @@
 #Create a file named bicycles.py that contains each of your classes.
 #Create a file named main.py that imports those classes, and uses them.
 
-class Bicycle(object):
-  def __init__(self, model, weight, cost, quantity):
+class Wheel(Bicycle):
+  def __init__(self, model, weight, cost):
     self.model = model
     self.weight = weight
     self.cost = cost
-    self.quantity = quantity
+    
+class Frame(Bicycle):
+  def __init__(self, material, weight, cost):
+    self.material = material
+    self.weight = weight
+    self.cost = cost
+
+class Manufacturer(object):
+  def __init__(self, name):
+    self.name = name
   
+class Bicycle(manufacturer):
+  def __init__(self, model, frame, wheel):
+    self.model = model
+    self.frame = frame
+    self.wheel = wheel
+    
+    def Bicycle_weight(self):
+      return self.wheel.weight * 2 + self.frame.weight
+    self.weight = weight
+    self.cost = cost
+
+class Bicycle(object):
+    def __init__(self, f_wheel, r_wheel, frame, name, mfg):
+        self.f_wheel = f_wheel
+        self.r_wheel = r_wheel
+        self.frame = frame
+        self.name = name
+        self.mfg = mfg
+
+    def weight(self):
+        return self.f_wheel.weight + self.r_wheel.weight + self.frame.weight
+
+    def wholesale_cost(self):
+        material_cost = self.f_wheel.cost + self.r_wheel.cost + self.frame.cost
+        return  material_cost + material_cost * (self.mfg.margin/100)    
+
+
 class BikeShop(object):
   def __init__(self, name, inventory, margin):
     self.name = name
--- bicycle/main.py
+++ bicycle/main.py
@@ -1,13 +1,8 @@
-import bicycles
+from bicycles import BikeShop, Bicycle, Customer, Wheel, Frame
 
 # Have each of the three customers purchase a bike then print the name of the bike the customer purchased, the cost, and how much money they have left over in their bicycle fund.
 
 #After each customer has purchased their bike, the script should print out the bicycle shop's remaining inventory for each bike, and how much profit they have made selling the three bikes.
-
-# redesign:
-# create individual instances of each class
-#  - instance of store, each of 6 bicycles, each of 3 customers
-#  - Do classes need to be updated?
   
 #  - Ask question: what would you like to do?
 #      [1] buy or [2] leave
@@ -16,7 +11,65 @@ import bicycles
 #      - update inventory, update their budget, update profit on sale
       
 #Create a Bicycle Shop
-def initialize_shop(shopname):
-  shopname = BikeShop(shopname, None, 20)
-  print "Welcome to %s" %shopname.name
-  return shopname
+Store101 = BikeShop("Mike's Bikes", None, 20)
+#Create 6 models of bike
+Bike1 = Bicycle("PureFix Papa", 20, 337, 10)
+Bike2 = Bicycle("PureFix Coolidge", 20, 299, 10)
+Bike3 = Bicycle("Diamondback Century 5", 19, 999, 10)
+Bike4 = Bicycle("Diamondback Interval Carbon", 18, 500, 10)
+Bike5 = Bicycle("Schwinn Huntington", 28, 105, 10)
+Bike6 = Bicycle("Schwinn Starlet", 25, 220, 10)
+# Create 3 customers
+Cust1 = Customer("Larry", "200", None)
+Cust2 = Customer("Moe", "500", None)
+Cust3 = Customer("Curly", "1000", None)
+# Create 3 wheels
+Wheel1 = Wheel("700C", 5, 30)
+Wheel2 = Wheel("26", 7, 35)
+Wheel3 = Wheel("20", 5, 20)
+# Create 3 frames
+Frame1 = Frame("aluminum", 3, 150)
+Frame2 = Frame("carbon", 2, 500)
+Frame3 = Frame("steel", 5, 75)
+
+Bike1.weight = Frame1.weight + (2 * Wheel1.weight)
+Bike2.weight = Frame1.weight + (2 * Wheel1.weight)
+Bike3.weight = Frame2.weight + (2 * Wheel1.weight)
+Bike4.weight = Frame2.weight + (2 * Wheel1.weight)
+Bike5.weight = Frame3.weight + (2 * Wheel3.weight)
+Bike6.weight = Frame1.weight + (2 * Wheel2.weight)
+
+Bike1.cost = Frame1.cost + (2 * Wheel1.cost)
+Bike2.cost = Frame1.cost + (2 * Wheel1.cost)
+Bike3.cost = Frame2.cost + (2 * Wheel1.cost)
+Bike4.cost = Frame2.cost + (2 * Wheel1.cost)
+Bike5.cost = Frame3.cost + (2 * Wheel3.cost)
+Bike6.cost = Frame1.cost + (2 * Wheel2.cost)
+
+
+
+#Print the name of each customer, and a list of the bikes offered by the bike shop that they can afford given their budget. 
+def evaluate_budget(Customer_list, storename):
+  Customer_list = []
+  Customer_list[0].append(Cust1)
+
+  for customer in Customer_list:
+    i = 0 
+    item_price = 0
+    print "The budget of %s is %d" %(customer.name, int(customer.budget))
+    while i < len(storename.Inventory):
+      item_price = float(storename.Inventory[i].cost) * float(1+(storename.margin/100.0))
+      if int(customer.budget) > item_price:
+        print customer.name, "You can afford the %s which sells for %d " %(storename.Inventory[i].model, item_price)
+      else:
+        #print customer.name, "You cannot afford the %s which costs %d" %(storename.Inventory[i].model, item_price)
+        pass
+      i += 1 
+
+def main():
+  pass
+
+  
+  
+if __name__ == "__main__":
+  main()
